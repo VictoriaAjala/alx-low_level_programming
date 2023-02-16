e2ff123992cdf057b286c757b003001f39a77253
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-02-16 14:55:32 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int main(void)
-{
-	/**
+/**
  * main - Entry point prints puts function
  *
  * Description: 'Meaning of programming'
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
+{	
 
 	puts("\"Programming is like building a multilingual puzzle");
 
