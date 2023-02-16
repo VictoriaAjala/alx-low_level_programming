60d00333ddd99493e3cb944c4b5bd3f3ff221186
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-02-16 14:31:53 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 
-	return 0;
+	return (0);
 
 }
