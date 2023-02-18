953b1e5eb525fc452b94e1c297630093f995a088
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-02-18 05:39:07 -0800

    committed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: alx variables if else while
+ * return: Always Return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n > 0){
+	printf("%d is positive", n);
+	}
+	else if (n < 0) {
+		printf("%d is negative", n);
+	}
+	else {
+		printf("%d is zero", n);
+	}
+	printf("\n");
+	return (0);
+}
