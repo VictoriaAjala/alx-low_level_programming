aa1e0d7bc7651ab358ee3d69aa7bdff35d98ad52
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-02-19 13:07:30 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: ALways Return 0
+ */
+
+int main(void)
+{
+	int num1, num2;
+	int num3 = 0;
+
+	if (num3 < 10)
+	{
+
+		for (num1 = 0; num1 < 9; num1++)
+		{
+			for (num2 = num3; num2 < 10; num2++)
+			{
+				if (num1 != num2)
+				{
+					putchar((num1) + '0');
+					putchar((num2) + '0');
+
+					if (num1 != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+			num3++;
+		}
+	}
+
+	return (0);
+}
